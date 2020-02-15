f1c8324038cb22ce4b18ed2a9dee61faf99bf25e
Alejandro F <progressandro@gmail.com>
2020-02-15 15:34:58 -0400

    Newton method updated.

--- newton.py
+++ newton.py
@@ -7,14 +7,12 @@ def metodo_newton(f, df, x0, e):
     x0 = x0 - f(x0)/df(x0)
     delta = f(x0)
     resultados.append([x0, f(x0), df(x0)])
-    print 'Raiz encontrada: ', x0
-    print 'f(x) en la raiz: ', f(x0)
   print(tabulate(resultados, headers=['x0', 'f(x0)', 'df(x0)']))
 
-def f(x):
-  return -(math.sin(x)/5) + x - (4/5)
+def f(t):
+  return 1 - math.exp(-1 * (math.pow(t, 2) - 12.15 * t + 36.875)) - math.exp(-1 * (math.pow(t, 2) - 7.4 * t + 13.2))
 
-def df(x):
-  return 1 - (math.cos(x)/5)
+def df(t):
+  return -(243/20-2*t)*math.exp((math.pow(t,2)+(243*t)/20-295/8)-(37/5-2*t))*math.exp((math.pow(-t,2)+(37*t)/5-66/5))
 
-metodo_newton(f, df, math.pi/2, 10**-6)
+metodo_newton(f, df, 3.6, 10**-7)
